d5fd5d17cf514f04c1747044c88e8250f495d2b1
Farhan Ishrak Islam <niloy28@live.com>
2024-06-13 17:20:19 +0900

    Fix hour display

--- src/components/HourlyWeather.vue
+++ src/components/HourlyWeather.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full max-w-screen-md py-12">
     <div class="mx-8 text-white">
-      <h2 class="mb-4">Hourly Weather</h2>
+      <h2 class="mb-4 text-2xl">Hourly Weather</h2>
       <div class="flex gap-10 overflow-x-scroll">
         <div
           v-for="hourlyData in hourlyWeather"
@@ -9,7 +9,7 @@
           class="flex flex-col items-center gap-4"
         >
           <HourlyWeatherCard
-            :time="hourlyData.dt"
+            :time="hourlyData.localTime"
             :icon="hourlyData.weather[0].icon"
             :temp="hourlyData.temp"
           />
