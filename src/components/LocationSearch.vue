3a2859c7ecc0fba20acb54c19124a52414449b02
Farhan Ishrak Islam <niloy28@live.com>
2025-01-28 15:29:40 +0900

    Fix location search

--- src/components/LocationSearch.vue
+++ src/components/LocationSearch.vue
@@ -1,48 +1,45 @@
 <template>
-    <div class="relative mb-8 pt-4">
-        <input
-            type="text"
-            v-model="searchQuery"
-            placeholder="Search for a city"
-            class="w-full border-b bg-transparent px-4 py-2 focus:border-weather-secondary focus:shadow-[0px_1px_0_0_#004E71] focus:outline-none"
-            name="location-search"
-            id="location-search"
-            @input="
-                () => {
-                    getSearchResults()
-                    fetchedLocations = null
-                }
-            "
-        />
-        <ul
-            v-if="fetchedLocations"
-            class="absolute top-[66px] w-full bg-weather-secondary p-2 text-white shadow-md"
+  <div class="relative mb-8 pt-4">
+    <input
+      type="text"
+      v-model="searchQuery"
+      placeholder="Search for a city"
+      class="w-full border-b bg-transparent px-4 py-2 focus:border-weather-secondary focus:shadow-[0px_1px_0_0_#004E71] focus:outline-none"
+      name="location-search"
+      id="location-search"
+      @input="
+        () => {
+          getSearchResults()
+          fetchedLocations = null
+        }
+      "
+    />
+    <ul
+      v-if="fetchedLocations"
+      class="absolute top-[66px] w-full bg-weather-secondary p-2 text-white shadow-md"
+    >
+      <p v-if="searchError">
+        Sorry, an error occurred while searching for the location. Please try
+        again later.
+      </p>
+      <p
+        v-if="!searchError && fetchedLocations && fetchedLocations.length == 0"
+      >
+        No matching locations found.
+      </p>
+      <template v-else>
+        <li
+          v-for="fetchedLocation in fetchedLocations"
+          :key="fetchedLocation.lat"
+          class="cursor-pointer px-1 py-2"
+          @click="previewWeather(fetchedLocation)"
         >
-            <p v-if="searchError">
-                Sorry, an error occurred while searching for the location.
-                Please try again later.
-            </p>
-            <p
-                v-if="
-                    !searchError &&
-                    fetchedLocations &&
-                    fetchedLocations.length == 0
-                "
-            >
-                No matching locations found.
-            </p>
-            <template v-else>
-                <li
-                    v-for="fetchedLocation in fetchedLocations"
-                    :key="fetchedLocation.id"
-                    class="cursor-pointer px-1 py-2"
-                    @click="previewWeather(fetchedLocation)"
-                >
-                    {{ fetchedLocation.place_name }}
-                </li>
-            </template>
-        </ul>
-    </div>
+          {{ fetchedLocation.name }}, {{ fetchedLocation.state }},
+          {{ fetchedLocation.country }}
+        </li>
+      </template>
+    </ul>
+  </div>
 </template>
 
 <script setup>
@@ -58,48 +55,52 @@ const searchError = ref(null)
 const router = useRouter()
 
 const getSearchResults = () => {
-    clearTimeout(queryTimeout.value)
-    queryTimeout.value = setTimeout(async () => {
-        if (searchQuery.value !== '') {
-            try {
-                fetchedLocations.value = await fetchLocations()
-                searchError.value = null
-            } catch (error) {
-                console.error(error)
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      try {
+        fetchedLocations.value = await fetchLocations()
+        searchError.value = null
+      } catch (error) {
+        console.error(error)
 
-                searchError.value = error
-            }
-        } else {
-            fetchLocations.value = null
-        }
-    }, 300)
+        searchError.value = error
+      }
+    } else {
+      fetchLocations.value = null
+    }
+  }, 300)
 }
 
 const fetchLocations = async () => {
-    const response = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${
-            searchQuery.value
-        }.json?access_token=${import.meta.env.VITE_MAPBOX_KEY}&types=place`
-    )
+  const response = await axios.get(
+    `${import.meta.env.VITE_OPENWEATHER_GEOCODING_BASE_URL}?q=${
+      searchQuery.value
+    }&limit=5&appid=${import.meta.env.VITE_OPENWEATHER_API_KEY}`
+  )
 
-    return response.data.features
+  return response.data
 }
 
 const previewWeather = (location) => {
-    const [city, state, country] = location.place_name.split(',')
+  const [city, state, country] = [
+    location.name,
+    location.state,
+    location.country,
+  ]
 
-    router.push({
-        name: 'weatherView',
-        params: {
-            country: country.trim(),
-            state: state.trim(),
-            city: city.trim(),
-        },
-        query: {
-            lon: location.geometry.coordinates[0],
-            lat: location.geometry.coordinates[1],
-            preview: true,
-        },
-    })
+  router.push({
+    name: 'weatherView',
+    params: {
+      country: country.trim(),
+      state: state.trim(),
+      city: city.trim(),
+    },
+    query: {
+      lon: location.lon,
+      lat: location.lat,
+      preview: true,
+    },
+  })
 }
 </script>
